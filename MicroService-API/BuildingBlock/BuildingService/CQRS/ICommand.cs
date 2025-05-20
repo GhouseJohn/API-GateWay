838f3f2231d883fbb7678395a60b366a2ec6de14
GhouseJohn <md.gosoddin@gmail.com>
2025-05-21 00:04:43 +0530

    'CQRS-Arch-designed'

---
+++ MicroService-API/BuildingBlock/BuildingService/CQRS/ICommand.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+
+namespace BuildingService.CQRS;
+
+/// <summary>
+/// ICommand is a shortcut for ICommand<Unit>, meaning it does not return any meaningful result—just like a "fire-and-forget" command.
+/// </summary>
+/// <typeparam name="TResponse"></typeparam>
+public interface ICommand : ICommand<Unit>
+{
+
+}
+
+/// <summary>
+/// ICommand<TResponse> extends IRequest<TResponse> from MediatR, which means any 
+/// ICommand<T> is a request that expects a response of type T
+/// </summary>
+public interface ICommand<out TResponse> : IRequest<TResponse>
+{
+}
